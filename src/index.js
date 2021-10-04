4057865ab26dcd36e65a977223e09f4571c17a40
Lana <sitdownrightnow2552@gmail.com>
2021-10-05 00:18:06 +0700

    feat: add rename log feature

--- src/index.js
+++ src/index.js
@@ -35,7 +35,41 @@ const argv = yargs(hideBin(process.argv))
         boolean: true,
         default: false,
     })
+    .option('log', {
+        describe: 'Log all changes applied',
+        boolean: true,
+        default: true,
+    })
+    .option('revert', {
+        describe: 'Revert latest change',
+        boolean: true,
+        default: false,
+    })
     .argv;
+
+const logPath = 'renamed.log';
+const logs = openLogs();
+
+if (argv.revert) {
+    revert();
+} else {
+    main();
+}
+
+
+function openLogs() {
+    if (!argv.log) return [];
+    try {
+        if (!fs.existsSync(logPath)) fs.writeFileSync(logPath, '[]', 'utf-8');
+        const raw = fs.readFileSync(logPath, 'utf-8');
+        const logs = JSON.parse(raw);
+        if (!Array.isArray(logs)) throw new Error('Invalid logs format');
+        return logs;
+    } catch {
+        console.error('Cannot read log file, please check logs file json format or disable log with --log=false');
+        process.exit(1);
+    }
+}
 
 function rename(s) {
     if (s == null || s.trim() === '') return;
@@ -43,15 +77,58 @@ function rename(s) {
     return argv.regex ? s.replace(new RegExp(argv.match), argv.rename) : s.replace(argv.match, argv.rename);
 }
 
-console.log(`Scanning dir ${argv.dir}`);
-const files = fs.readdirSync(argv.dir);
-files.forEach(name => {
-    const newName = rename(name);
-    if (newName === name) return;
+function revert() {
+    if (logs.length === 0) {
+        console.log('Nothing to revert');
+        return;
+    }
 
-    const file = path.resolve(argv.dir, name);
-    const newFile = path.resolve(argv.dir, newName);
-    fs.renameSync(file, newFile);
-    console.log(`Match ${name} => ${newName}`);
-});
-console.log(`Batch rename completed ${argv.dir}`);
+    const log = logs.pop();
+    if (log.length === 0) {
+        console.log('Nothing to revert');
+        return;
+    }
+
+    const errors = [];
+    for (const entry of log) {
+        try {
+            fs.renameSync(entry.newFile, entry.file);
+            console.log(`Revert ${entry.newFile} => ${entry.file}`);
+        } catch {
+            errors.push(entry);
+            console.log(`Cannot revert entry ${entry.newFile}`);
+        }
+    }
+    if (errors.length > 0) logs.push(errors);
+    fs.writeFileSync(logPath, JSON.stringify(logs, null, 2), 'utf-8');
+}
+
+function main() {
+    console.log(`Scanning dir ${argv.dir}`);
+    const files = fs.readdirSync(argv.dir);
+
+    if (files.length <= 0) {
+        console.log(`No match file in ${argv.dir}`);
+        process.exit(0);
+    }
+
+    const log = [];
+    logs.push(log);
+    try {
+        for (const name of files) {
+            if (name === logPath) continue;
+
+            const newName = rename(name);
+            if (newName === name) continue;
+
+            const file = path.resolve(argv.dir, name);
+            const newFile = path.resolve(argv.dir, newName);
+            fs.renameSync(file, newFile);
+            log.push({ file, newFile, time: new Date().toISOString() });
+            console.log(`Match ${name} => ${newName}`);
+        }
+    } finally {
+        if (argv.log && log.length > 0) fs.writeFileSync(logPath, JSON.stringify(logs, null, 2), 'utf-8');
+    }
+    console.log(`Batch rename completed ${argv.dir}`);
+}
