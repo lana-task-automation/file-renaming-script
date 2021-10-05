171108018f5adb5b5c379b10830ffb0eb6b5db85
Lana <sitdownrightnow2552@gmail.com>
2021-10-06 01:05:52 +0700

    build: update rollup bundle for handling typescript files

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: {
         file: 'build/renamer.cjs.js',
         format: 'cjs',
         exports: 'auto',
     },
     plugins: [
+        typescript(),
         nodeResolve(),
         commonjs(),
         terser(),
